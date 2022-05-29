dee9fdb690ff6e9dcef69e9c53bfc336a6baf68c
Pedro Roque <89155205+pedrohsroque@users.noreply.github.com>
2022-05-29 13:13:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as sl
 
+sl.title('My Parents New Health Diner')
+
+sl.header('Breakfast Menu')
+sl.text('Omega 3 & Blueberry Oatmeal')
+sl.text('Kale Spinach & Rocket Smoothie')
+sl.text('Hard-Boiled Free-Range Egg')
